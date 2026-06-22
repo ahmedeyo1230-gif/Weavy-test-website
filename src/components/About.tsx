657d71e11dc1c14b84cba64bf6b8065b359fddce
Hazzy G <hazzy_g@hazzys-mbp.home>
2026-06-22 22:52:47 +0100

    Revert "Add animated noise + grid background to About section (3rd section)"
    This reverts commit b17016c28cca2fb4a979334a1fd8e88adff77cd4.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion'
-import { NoiseSectionBg } from './ui/noise-background'
 
 const E: [number, number, number, number] = [0.16, 1, 0.3, 1]
 
@@ -13,7 +12,7 @@ const SERVICE_MODULES = [
   { num: '05', label: 'Video Editing',   desc: 'Storytelling that drives results'  },
 ]
 
-export default function About() {
+export default function About() { // no diagonal lines bg
   return (
     <section
       id="about"
@@ -25,9 +24,6 @@ export default function About() {
           '#0B1114',
       }}
     >
-      {/* Noise + faded grid + cyan spotlight background */}
-      <NoiseSectionBg />
-
       {/* Dot grid */}
       <div
         className="absolute inset-0 pointer-events-none"
