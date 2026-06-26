308fdda8b231622fb17e22e28516668e84c6ff39
Hazzy G <hazzy_g@hazzys-mbp.home>
2026-06-26 03:12:55 +0100

    Reduce work hero size by 10% on mobile

--- src/components/ImageShowcase.tsx
+++ src/components/ImageShowcase.tsx
@@ -1404,7 +1404,7 @@ export default function ImageShowcase() {
           loop
           playsInline
           preload="auto"
-          className="object-cover object-top h-[73vw] sm:h-[73vw] lg:h-auto lg:object-contain lg:object-center"
+          className="object-cover object-top h-[66vw] sm:h-[66vw] lg:h-auto lg:object-contain lg:object-center"
           style={{
             display: 'block',
             width: '100%',
