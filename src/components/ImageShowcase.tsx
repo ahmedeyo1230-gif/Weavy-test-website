5d5381b748312eaacac7eb8ef3f52d760aa37dec
Hazzy G <hazzy_g@hazzys-mbp.home>
2026-06-26 03:03:32 +0100

    Increase work hero size on mobile

--- src/components/ImageShowcase.tsx
+++ src/components/ImageShowcase.tsx
@@ -1404,7 +1404,7 @@ export default function ImageShowcase() {
           loop
           playsInline
           preload="auto"
-          className="object-contain object-top lg:h-auto lg:object-contain lg:object-center"
+          className="object-cover object-top h-[73vw] sm:h-[73vw] lg:h-auto lg:object-contain lg:object-center"
           style={{
             display: 'block',
             width: '100%',
