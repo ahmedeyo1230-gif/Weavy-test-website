671a292fca9e0c117d3426887eefcec7be11d322
Hazzy G <hazzy_g@unknown.home>
2026-06-27 02:23:35 +0100

    Remove dark overlay from bespoke section mockup

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -10635,12 +10635,6 @@ export default function Services() {
                   aria-label="Bespoke website design showcase"
                   style={{ display: 'block', width: '100%', height: 'auto', objectFit: 'cover' }}
                 />
-                {/* Bottom vignette */}
-                <div aria-hidden="true" style={{
-                  position: 'absolute', bottom: 0, left: 0, right: 0, height: '32%',
-                  background: 'linear-gradient(to top, hsl(210 20% 3% / 0.65) 0%, transparent 100%)',
-                  pointerEvents: 'none',
-                }}/>
               </div>
 
             </div>
